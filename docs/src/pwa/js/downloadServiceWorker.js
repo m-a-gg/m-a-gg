6a859eb88de043542c481fcc7ed3fa18de6b94a1
M&A ADMIN <m.a.gizmogurus@gmail.com>
2024-03-03 15:30:02 +0000

    TEST V2
    Signed-off-by: M&A ADMIN <m.a.gizmogurus@gmail.com>

---
+++ docs/src/pwa/js/downloadServiceWorker.js
@@ -0,0 +1,15 @@
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice() && 'serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+})
+.catch(error => {
+    console.error('Service Worker registration failed:', error);
+});
+});
+}
